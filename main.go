ebc1ae0e074297d405cd9471e2bde9818d3a4bbf
umiatiyah <umiatiyah97@gmail.com>
2022-06-22 21:50:09 +0700

    add permission cors

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"log"
 	"main/server"
 	"net/http"
+	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -13,7 +15,10 @@ func main() {
 	r := mux.NewRouter()
 
 	server.InitializeRoute(r)
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
+	originsOk := handlers.AllowedOrigins([]string{os.Getenv("ORIGIN_ALLOWED")})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(originsOk, headersOk, methodsOk)(r)))
 
 }
