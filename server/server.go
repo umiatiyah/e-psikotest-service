37cc0b5556f962a1a90a3e0884e4621db55f0561
umiatiyah <umiatiyah97@gmail.com>
2022-07-28 15:23:37 +0700

    update endpoint

--- server/server.go
+++ server/server.go
@@ -21,11 +21,11 @@ func InitializeRoute(r *mux.Router) {
 	r.HandleFunc("/admin/update/{id}", auth.MiddlewareAuth(admin.UpdateAdmin)).Methods("POST")
 	r.HandleFunc("/admin/delete/{id}", auth.MiddlewareAuth(admin.DeleteAdmin)).Methods("DELETE")
 
-	r.HandleFunc("/admin/user", auth.MiddlewareAuth(user.GetUsers)).Methods("GET")
-	r.HandleFunc("/admin/user/{id}", auth.MiddlewareAuth(user.GetUser)).Methods("GET")
-	r.HandleFunc("/admin/user", auth.MiddlewareAuth(user.AddUser)).Methods("POST")
-	r.HandleFunc("/admin/user/{id}", auth.MiddlewareAuth(user.UpdateUser)).Methods("POST")
-	r.HandleFunc("/admin/user/{id}", auth.MiddlewareAuth(user.DeleteUser)).Methods("DELETE")
+	r.HandleFunc("/admin/user", auth.MiddlewareAuth(admin.GetUsers)).Methods("GET")
+	r.HandleFunc("/admin/user/{id}", auth.MiddlewareAuth(admin.GetUser)).Methods("GET")
+	r.HandleFunc("/admin/user", auth.MiddlewareAuth(admin.AddUser)).Methods("POST")
+	r.HandleFunc("/admin/user/{id}", auth.MiddlewareAuth(admin.UpdateUser)).Methods("POST")
+	r.HandleFunc("/admin/user/{id}", auth.MiddlewareAuth(admin.DeleteUser)).Methods("DELETE")
 
 	r.HandleFunc("/admin/category", auth.MiddlewareAuth(admin.GetCategories)).Methods("GET")
 	r.HandleFunc("/admin/category/{id}", auth.MiddlewareAuth(admin.GetCategory)).Methods("GET")
@@ -45,5 +45,13 @@ func InitializeRoute(r *mux.Router) {
 	r.HandleFunc("/admin/answer/{id}", auth.MiddlewareAuth(admin.UpdateAnswer)).Methods("POST")
 	r.HandleFunc("/admin/answer/{id}", auth.MiddlewareAuth(admin.DeleteAnswer)).Methods("DELETE")
 
+	r.HandleFunc("/admin/history", auth.MiddlewareAuth(admin.GetHistory)).Methods("GET")
+	r.HandleFunc("/admin/valuation", auth.MiddlewareAuth(admin.GetValuation)).Methods("GET")
+
+	r.HandleFunc("/user/detail/{id}", auth.MiddlewareAuth(admin.GetUser)).Methods("GET")
+	r.HandleFunc("/user/update/{id}", auth.MiddlewareAuth(admin.UpdateUser)).Methods("POST")
+
 	r.HandleFunc("/user/question", auth.MiddlewareAuth(user.GetQuestions)).Methods("GET")
+	r.HandleFunc("/user/history", auth.MiddlewareAuth(user.SaveHistory)).Methods("POST")
+	r.HandleFunc("/user/historyV2", auth.MiddlewareAuth(user.AddValuation)).Methods("POST")
 }
