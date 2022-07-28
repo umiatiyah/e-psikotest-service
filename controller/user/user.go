a07a70c248e470a024407c0537753bbe6e487fc6
umiatiyah <umiatiyah97@gmail.com>
2022-07-28 15:22:20 +0700

    remove some function in user side

--- controller/user/user.go
+++ controller/user/user.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"main/controller"
 	"main/controller/auth"
 	"main/model"
@@ -18,37 +17,6 @@ import (
 
 	"github.com/gorilla/mux"
 )
-
-func GetUsers(w http.ResponseWriter, r *http.Request) {
-
-	rows, err := utils.DB.Query("SELECT id, name, email, nik FROM users ORDER BY id asc")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_, role, err := auth.ExtractTokenID(r)
-	if err != nil || role != utils.Adm {
-		w.Header().Set("Content-Type", "application/json")
-		response.ERROR(w, http.StatusUnauthorized, errors.New(http.StatusText(http.StatusUnauthorized)))
-		return
-	}
-
-	var users []response.UserListResponse
-
-	for rows.Next() {
-		var user response.UserListResponse
-		rows.Scan(&user.Id, &user.Name, &user.Email, &user.NIK)
-
-		users = append(users, user)
-	}
-
-	peopleBytes, _ := json.MarshalIndent(users, "", "\t")
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(peopleBytes)
-
-	defer rows.Close()
-}
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
@@ -70,63 +38,6 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(peopleBytes)
 
-}
-
-func AddUser(w http.ResponseWriter, r *http.Request) {
-	var user model.User
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-	}
-
-	_, role, err := auth.ExtractTokenID(r)
-	if err != nil || role != utils.Adm {
-		w.Header().Set("Content-Type", "application/json")
-		response.ERROR(w, http.StatusUnauthorized, errors.New(http.StatusText(http.StatusUnauthorized)))
-		return
-	}
-
-	sqlCekUser := query.SqlQueryCek(utils.Usr)
-	exist := controller.CekUser(user.Email, sqlCekUser)
-
-	if exist.Email != "" {
-
-		user := response.UserResponse{
-			Name:  user.Name,
-			Email: user.Email,
-			NIK:   user.NIK,
-			Message: response.BaseResponse{
-				Status:  http.StatusOK,
-				Message: "Email Telah Digunakan oleh " + exist.Name,
-			},
-		}
-		peopleBytes, _ := json.MarshalIndent(user, "", "\t")
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(peopleBytes)
-
-	} else {
-
-		sqlStatement := `INSERT INTO users (name, email, nik, password, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6)`
-		_, err = utils.DB.Exec(sqlStatement, user.Name, user.Email, user.NIK, utils.HashAndSalt([]byte(user.Password)), time.Now(), time.Now())
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			panic(err)
-		}
-
-		user := response.UserResponse{
-			Name:  user.Name,
-			Email: user.Email,
-			NIK:   user.NIK,
-			Message: response.BaseResponse{
-				Status:  http.StatusOK,
-				Message: "User Created!",
-			},
-		}
-		peopleBytes, _ := json.MarshalIndent(user, "", "\t")
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(peopleBytes)
-
-	}
 }
 
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
@@ -189,37 +100,6 @@ func UpdateUser(w http.ResponseWriter, r *http.Request) {
 		w.Write(peopleBytes)
 
 	}
-}
-
-func DeleteUser(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.ParseUint(vars["id"], 10, 32)
-	if err != nil {
-		response.ERROR(w, http.StatusBadRequest, err)
-		return
-	}
-
-	_, role, err := auth.ExtractTokenID(r)
-	if err != nil || role != utils.Adm {
-		w.Header().Set("Content-Type", "application/json")
-		response.ERROR(w, http.StatusUnauthorized, errors.New(http.StatusText(http.StatusUnauthorized)))
-		return
-	}
-
-	sqlStatement := `DELETE FROM users WHERE id = $1`
-	_, err = utils.DB.Exec(sqlStatement, id)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		panic(err)
-	}
-	user := response.BaseResponse{
-		Status:  http.StatusOK,
-		Message: "User Deleted!",
-	}
-	peopleBytes, _ := json.MarshalIndent(user, "", "\t")
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(peopleBytes)
-
 }
 
 func LoginUser(w http.ResponseWriter, r *http.Request) {
