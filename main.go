ac30a67eac88eb0e2a9a86cb96768051b6c62714
umiatiyah <umiatiyah97@gmail.com>
2022-07-29 22:18:19 +0700

    update cors config

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -15,10 +15,8 @@ func main() {
 	r := mux.NewRouter()
 
 	server.InitializeRoute(r)
-	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
-	originsOk := handlers.AllowedOrigins([]string{os.Getenv("ORIGIN_ALLOWED")})
-	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+	handle := cors.AllowAll().Handler(r)
 
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), handlers.CORS(originsOk, headersOk, methodsOk)(r)))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), handle))
 
 }
