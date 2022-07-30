659db28dcc3c25d911cc0f7997c2097969648d6a
umiatiyah <umiatiyah97@gmail.com>
2022-07-30 19:51:29 +0700

    add bobot score history

--- model/valuation.go
+++ model/valuation.go
@@ -7,8 +7,21 @@ type Valuation struct {
 	UpdatedAt string `json:"updated_at"`
 }
 
+type PreBobotValuation struct {
+	UserID     int `json:"user_id"`
+	CategoryID int `json:"category_id"`
+	Score      int `json:"score"`
+}
+
+type BobotValuation struct {
+	UserID     int    `json:"user_id"`
+	CategoryID int    `json:"category_id"`
+	TotalScore int    `json:"total_score"`
+	CreatedAt  string `json:"created_at"`
+	UpdatedAt  string `json:"updated_at"`
+}
+
 type PreValuation struct {
 	CategoryID int `json:"category_id"`
 	MinScore   int `json:"min_score"`
-	Score      int `json:"score"`
 }
